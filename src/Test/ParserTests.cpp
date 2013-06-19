6f530e765ec8cb7f15474f0c0f945247d1b25104
Paul Woolcock <pwoolcock@barracuda.com>
2013-06-19 15:30:09 -0400

    Add a more convenient way to test parsing

--- src/Test/ParserTests.cpp
+++ src/Test/ParserTests.cpp
@@ -11,16 +11,29 @@ namespace magpie
     parseModule();
   }
 
-  void ParserTests::parseModule()
+  // TODO: hacky, parses arbitrary code string into a module
+  void ParserTests::testCode(const char* sourceString, const char* expected)
   {
-    gc<String> code = String::create("def foo()\n1+2*3 and 4/5+6%7\nend");
+    gc<String> code = String::create(sourceString);
     ErrorReporter reporter;
     gc<SourceFile> source = new SourceFile(String::create("<file>"), code);
     Parser parser(source, reporter);
     gc<ModuleAst> module = parser.parseModule();
 
-    gc<String> text = module->toString();
-    EXPECT_EQUAL("((1 + (2 * 3)) and ((4 / 5) + (6 % 7)))", *text);
+    int numErrors = reporter.numErrors();
+
+    EXPECT_EQUAL(0, numErrors);
+
+    if (numErrors == 0) {
+        gc<String> text = module->body()->toString();
+        EXPECT_EQUAL(expected, *text);
+    }
+  }
+
+  void ParserTests::parseModule()
+  {
+    testCode("def foo()\n1+2*3 and 4/5+6%7\nend",
+             "(\n(def foo -> (and (1 + (2 * 3)) ((4 / 5) + (6 % 7))))\n)");
   }
 }
 
--- src/Test/ParserTests.h
+++ src/Test/ParserTests.h
@@ -11,6 +11,7 @@ namespace magpie
 
   private:
     void parseModule();
+    void testCode(const char*, const char*);
   };
 }
 
